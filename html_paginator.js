3cb114ba98fc5d0a87dfbf60cde6fe222e922d15
Gustavo Soto Ridd <gsoto@dcc.uchile.cl>
2016-04-17 19:41:07 -0300

    Now it accepts multipla paginators in the same document

--- html_paginator.js
+++ html_paginator.js
@@ -4,65 +4,68 @@
 
 Basic HTML structure
 
-.html_paginator .html_paginator_page
-.html_paginator .html_paginator_next
-.html_paginator .html_paginator_prev
+.html_paginator_page
+.html_paginator_next
+.html_paginator_prev
 
 **/
 (function ( $ ) {
 
-$.fn.htmlPaginator = function htmlPaginator(){
+  $.fn.htmlPaginator = function htmlPaginator(){
 
-  var nextClass = '.html_paginator_next';
-  var prevClass = '.html_paginator_prev';
+    return this.each(function(){
 
-  $('.html_paginator').each(function() {
-    var $paginator = $(this);
+      console.log("htmlPaginator");
+      var nextClass = '.html_paginator_next';
+      var prevClass = '.html_paginator_prev';
 
-    var $next_triggers = $paginator.find('.html_paginator_next');
-    var $prev_triggers = $paginator.find('.html_paginator_prev');
+      var $paginator = $(this);
 
-    if($next_triggers.size() === 0){
-      console.error("The following html_paginator element does not have an element with " + nextClass);
-      console.error($paginator);
-    }
+      var $next_triggers = $paginator.find('.html_paginator_next');
+      var $prev_triggers = $paginator.find('.html_paginator_prev');
 
-    if($prev_triggers.size() === 0){
-      console.error("The following html_paginator element does not have an element with " + nextClass);
-      console.error($paginator);
-    }
+      if($next_triggers.size() === 0){
+        console.error("The following html_paginator element does not have an element with " + nextClass);
+        console.error($paginator);
+      }
 
-    var page_divs = $paginator.find('.html_paginator_page');
-    var shown_index = 0;
-    var total_pages = page_divs.size();
+      if($prev_triggers.size() === 0){
+        console.error("The following html_paginator element does not have an element with " + nextClass);
+        console.error($paginator);
+      }
 
-    function showRespective(){
-      // Hide all elements
-      page_divs.map(function(){ $(this).hide() });
-      // Then show
-      $(page_divs[shown_index]).show();
-    }
+      var page_divs = $paginator.find('.html_paginator_page');
+      console.log(page_divs);
+      var shown_index = 0;
+      var total_pages = page_divs.size();
 
-    function nextPage(e){
-      e.preventDefault();
-      shown_index = (shown_index + 1) % total_pages;
+      function showRespective(){
+        // Hide all elements
+        page_divs.map(function(){ $(this).hide() });
+        // Then show
+        $(page_divs[shown_index]).show();
+      }
+
+      function nextPage(e){
+        e.preventDefault();
+        shown_index = (shown_index + 1) % total_pages;
+        showRespective();
+      }
+
+      function prevPage(e){
+        e.preventDefault();
+        shown_index = (shown_index + (total_pages -1) ) % total_pages;
+        showRespective();
+      }
+
+      $next_triggers.on('click', nextPage);
+      $prev_triggers.on('click', prevPage);
+
       showRespective();
+
+      // For chaining
+      return this;
+      });
+
     }
-
-    function prevPage(e){
-      e.preventDefault();
-      shown_index = (shown_index + (total_pages -1) ) % total_pages;
-      showRespective();
-    }
-
-    $next_triggers.on('click', nextPage);
-    $prev_triggers.on('click', prevPage);
-
-    showRespective();
-
-    // For chaining
-    return this;
-  });
-
-}
 }( jQuery ));
